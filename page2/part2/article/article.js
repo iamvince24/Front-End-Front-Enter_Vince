2e2d9e55cb84032dd110fd0cf626f7c58e70a180
iamvince24 <yohongguo24@gmail.com>
2023-12-01 12:40:12 +0800

    Adding get data, tag & Slide effect

--- page2/part2/article/article.js
+++ page2/part2/article/article.js
@@ -1,36 +1,178 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("loading-container").style.transform =
-      "translateY(-110%)";
-    document.querySelector(".color-block-1").style.transform = "scaleY(5)";
-    document.querySelector(".color-block-2").style.transform = "scaleY(3.5)";
-    setTimeout(function () {
-      document.querySelector(".color-block-1").style.transform = "scaleY(0)";
-      document.querySelector(".color-block-2").style.transform = "scaleY(0)";
-    }, 700);
-  }, 2000);
-});
+window.onload = function () {
+  setInterval(function () {
+    document.querySelector(".article-keyvisual").style.animationPlayState =
+      "running";
+  }, 5000);
+};
 
-const searchButton = document.querySelector("#search-icon");
-const searchContainerBlack = document.querySelector(".search-container-black");
+const articleListContainer = document.querySelector(".articlelist");
+const filterButtons = document.querySelectorAll(".filter-btn");
 
-function handleSearchContainer() {
-  var searchContainer = document.querySelector("#search-container");
+const fetchData = async () => {
+  const path = "../front-enter-export.json";
 
-  if (searchContainer.style.display === "none") {
-    searchContainer.style.display = "block";
-  } else {
-    searchContainer.style.display = "none";
+  try {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error(
+        `Network response was not ok, status: ${response.status}`
+      );
+    }
+
+    const data = await response.json();
+    const articleKeys = Object.keys(data.article);
+
+    for (const key in data.article) {
+      const articleCardList = data.article[key];
+      handleAppendChild(articleCardList);
+    }
+
+    filterButtons.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        if (event.target.classList.contains("filter-btn")) {
+          handleFilterClick(event.target);
+        }
+      });
+    });
+
+    function handleFilterClick(element) {
+      articleListContainer.innerHTML = "";
+      const filterCondition = element.innerHTML;
+      let filterData = [];
+
+      if (filterCondition === "全部") {
+        filterData = articleKeys;
+      } else if (filterCondition === "小班制") {
+        filterData = articleKeys.filter(
+          (key) => data.article[key].classType === filterCondition
+        );
+      } else if (filterCondition === "放養制") {
+        filterData = articleKeys.filter(
+          (key) => data.article[key].teachWay === filterCondition
+        );
+      } else if (filterCondition === "一對一") {
+        filterData = articleKeys.filter(
+          (key) => data.article[key].classType === filterCondition
+        );
+      } else if (
+        filterCondition === "台北" ||
+        filterCondition === "各地" ||
+        filterCondition === "高雄"
+      ) {
+        filterData = articleKeys.filter(
+          (key) => data.article[key].city === filterCondition
+        );
+      }
+
+      for (var i = 0; i < filterData.length; i++) {
+        const articleCardList = data.article[filterData[i]];
+        handleAppendChild(articleCardList);
+      }
+    }
+
+    function handleAppendChild(element) {
+      const { city, rectangleUrl, name, preface } = element;
+      const newArticleItem = document.createElement("article");
+      newArticleItem.classList.add("articlelist-card");
+
+      newArticleItem.innerHTML = `
+        <div class="location">
+          <img
+            class="location-icon"
+            src="../img/One-location-icon.png"
+            alt="location-icon"
+          />
+          <div class="click-effect filter-btn">${city}</div>
+        </div>
+        <div class="articlelist-content">
+          <div class="articlelist-pic-container">
+            <img
+              class="articlelist-pic"
+              src=${rectangleUrl}
+              alt="articlelist-picture"
+            />
+          </div>
+          <p class="title">${name}</p>
+          <div class="text">
+          ${preface}
+          </div>
+          <div class="articlelist-readmore">
+            <div class="readmore-word">read more</div>
+            <img
+              class="readmore-arrow"
+              src="../img/Arrow-right-one.png"
+              alt="readmore-arrow"
+            />
+          </div>
+        </div>
+      `;
+
+      articleListContainer.appendChild(newArticleItem);
+
+      const filterBtn = newArticleItem.querySelector(".filter-btn");
+      if (filterBtn) {
+        filterBtn.addEventListener("click", () => {
+          const filterCondition = city;
+          if (
+            filterCondition === "台北" ||
+            filterCondition === "各地" ||
+            filterCondition === "高雄"
+          ) {
+            articleListContainer.innerHTML = "";
+            var filterData = articleKeys.filter(
+              (key) => data.article[key].city === filterCondition
+            );
+
+            for (var i = 0; i < filterData.length; i++) {
+              const articleCardList = data.article[filterData[i]];
+
+              const { city, rectangleUrl, name, preface } = articleCardList;
+              const newArticleItem = document.createElement("article");
+              newArticleItem.classList.add("articlelist-card");
+
+              newArticleItem.innerHTML = `
+              <div class="location">
+                <img
+                  class="location-icon"
+                  src="../img/One-location-icon.png"
+                  alt="location-icon"
+                />
+                <div class="click-effect filter-btn">${city}</div>
+              </div>
+              <div class="articlelist-content">
+                <div class="articlelist-pic-container">
+                  <img
+                    class="articlelist-pic"
+                    src=${rectangleUrl}
+                    alt="articlelist-picture"
+                  />
+                </div>
+                <p class="title">${name}</p>
+                <div class="text">
+                ${preface}
+                </div>
+                <div class="articlelist-readmore">
+                  <div class="readmore-word">read more</div>
+                  <img
+                    class="readmore-arrow"
+                    src="../img/Arrow-right-one.png"
+                    alt="readmore-arrow"
+                  />
+                </div>
+              </div>
+            `;
+
+              articleListContainer.appendChild(newArticleItem);
+            }
+
+            articleListContainer.appendChild(newArticleItem);
+          }
+        });
+      }
+    }
+  } catch (error) {
+    console.error("錯誤:", error);
   }
-}
+};
 
-searchButton.addEventListener("click", handleSearchContainer);
-searchContainerBlack.addEventListener("click", handleSearchContainer);
-
-const topIcon = document.querySelector("#top-icon");
-topIcon.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
+fetchData();
--- page2/part2/script.js
+++ page2/part2/script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("loading-container").style.transform =
-      "translateY(-110%)";
-    document.querySelector(".color-block-1").style.transform = "scaleY(5)";
-    document.querySelector(".color-block-2").style.transform = "scaleY(3.5)";
-    setTimeout(function () {
-      document.querySelector(".color-block-1").style.transform = "scaleY(0)";
-      document.querySelector(".color-block-2").style.transform = "scaleY(0)";
-    }, 700);
-  }, 2000);
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   setTimeout(function () {
+//     document.getElementById("loading-container").style.transform =
+//       "translateY(-110%)";
+//     document.querySelector(".color-block-1").style.transform = "scaleY(5)";
+//     document.querySelector(".color-block-2").style.transform = "scaleY(3.5)";
+//     setTimeout(function () {
+//       document.querySelector(".color-block-1").style.transform = "scaleY(0)";
+//       document.querySelector(".color-block-2").style.transform = "scaleY(0)";
+//     }, 700);
+//   }, 2000);
+// });
 
 const searchButton = document.querySelector("#search-icon");
 const searchContainerBlack = document.querySelector(".search-container-black");

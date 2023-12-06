93d19b37f5de866d73b9f5fed8bc06cd3d3e194e
iamvince24 <yohongguo24@gmail.com>
2023-12-06 21:39:53 +0800

    Animation done

--- page3/part3/app.js
+++ page3/part3/app.js
@@ -107,11 +107,16 @@ const testSection = document.querySelector(".testSection");
 
 testGo.forEach((button) => {
   button.addEventListener("click", () => {
+    if (testCardList.style.display === "none") {
+      testCardStart.style.display = "flex";
+    }
     testSection.style.display = "flex";
   });
 });
 
 testSection.addEventListener("click", () => {
+  testCardList.style.display = "none";
+  testCardResult.style.display = "none";
   testSection.style.display = "none";
 });
 
@@ -120,17 +125,16 @@ const startBtn = document.querySelector(".start-btn");
 const testCardList = document.querySelector(".testCard-list");
 const testCardResult = document.querySelector(".testCard-result");
 const ChartScore = document.querySelector(".ChartScore");
-const resultBtn = document.querySelector(".resultBtn");
 
 const testQuestions = [
   {
     question: "選擇在哪座城市學習？",
-    stage: "1/5",
+    stage: "1 / 5",
     listOption: ["台北", "台中", "高雄", "各地", "不重要"],
   },
   {
     question: "每月能撥出多少費用學習？",
-    stage: "2/5",
+    stage: "2 / 5",
     listOption: [
       "3000元以下",
       "6000元內",
@@ -141,17 +145,17 @@ const testQuestions = [
   },
   {
     question: "每周能撥出多少時間學習？",
-    stage: "3/5",
+    stage: "3 / 5",
     listOption: ["16小時以下", "30小時內", "45小時內", "46小時以上", "不重要"],
   },
   {
     question: "對班制的需求是？",
-    stage: "4/5",
+    stage: "4 / 5",
     listOption: ["大班制", "小班制", "一對一", "不重要"],
   },
   {
     question: "喜歡什麼樣的教學方式？",
-    stage: "5/5",
+    stage: "5 / 5",
     listOption: ["放養制", "手把收教制", "不重要"],
   },
 ];
@@ -160,19 +164,19 @@ testCardStart.addEventListener("click", (event) => {
   event.stopPropagation();
 });
 
+testCardList.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
 startBtn.addEventListener("click", async () => {
   testCardStart.style.display = "none";
   testCardList.style.display = "flex";
-  testCardList.addEventListener("click", (event) => {
-    event.stopPropagation();
-  });
 
   var testCardIndex = 0;
   const answersArray = [];
   await setTestCard(testCardIndex, answersArray);
   const testFilterCondition = answersArray;
   const testResultKey = await getTestResult(testFilterCondition);
-  console.log(testResultKey);
 
   const data = await fetchJsonData("../front-enter-export.json");
   const { city, fee, weekHour, classType, teachWay } =
@@ -184,10 +188,69 @@ startBtn.addEventListener("click", async () => {
   );
   const commonScore = commonElement.length / testArray.length;
   const comPercentage = (commonScore * 100).toFixed(0) + "%";
+  const commonScoreDeg = commonScore * 360;
 
-  ChartScore.innerHTML = comPercentage;
-  resultBtn.innerHTML = data.article[testResultKey].name;
+  // 圓餅圖
+  const resultScoreChart = document.querySelector(".resultScore-Chart");
 
+  resultScoreChart.addEventListener("animationend", () => {
+    resultScoreChart.style.background =
+      "conic-gradient(" +
+      `var(--dark-color) 0deg ${commonScoreDeg}deg,` +
+      `var(--primary-color) ${commonScoreDeg}deg 360deg` +
+      ")";
+  });
+
+  // number
+  let currentPercentage = 10;
+  let cyclesCompleted = 0;
+
+  const intervalIdChart = setInterval(() => {
+    ChartScore.innerHTML = `${currentPercentage}%`;
+    currentPercentage += 10;
+    if (currentPercentage > 100) {
+      currentPercentage = 10;
+      cyclesCompleted++;
+      if (cyclesCompleted === 2) {
+        clearInterval(intervalIdChart);
+
+        ChartScore.innerHTML = comPercentage;
+      }
+    }
+  }, 2000 / 20);
+
+  //Btn text
+  const resultBtn = document.querySelector(".resultBtn");
+  function displayText(index) {
+    resultBtn.textContent = classList[index];
+  }
+
+  const texts = [
+    "彭彭的課程教學",
+    "飛肯設計學苑",
+    "台大資訊系統訓練班",
+    "赫綵電腦",
+    "AppWorks School",
+    "六角學院",
+    "五倍紅寶石",
+    "Udemy",
+    "UXabc",
+  ];
+
+  let index = 0;
+  function displayText() {
+    resultBtn.textContent = texts[index];
+    index = (index + 1) % texts.length;
+  }
+  const intervalId = setInterval(() => {
+    displayText();
+  }, 150);
+  setTimeout(() => {
+    clearInterval(intervalId);
+    resultBtn.innerHTML = data.article[testResultKey].name;
+  }, 2000);
+
+  // set Url
   resultBtn.addEventListener("click", () => {
     const contentUrl =
       window.location.protocol + "//" + window.location.host + "/content.html";
@@ -314,15 +377,12 @@ function setTestCard(i, answersArray) {
         i += 1;
 
         if (i <= 4) {
-          // 遞歸調用 setTestCard，並在點擊事件處理完成後 resolve Promise
           setTestCard(i, answersArray).then(() => {
             resolve();
           });
         } else {
-          // console.log(answersArray);
           testCardResult.style.display = "flex";
           testCardList.style.display = "none";
-          // 在最後一次點擊事件處理完成後 resolve Promise
           resolve();
         }
       });

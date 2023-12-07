8312ce70bd3bcecf881133f3542e9578a89f1ea3
iamvince24 <yohongguo24@gmail.com>
2023-12-07 21:37:38 +0800

    adjust some animation

--- page3/part3/scripts/app.js
+++ page3/part3/scripts/app.js
@@ -5,18 +5,18 @@ import {
 } from "./utils.js";
 
 // loading handle
-// document.addEventListener("DOMContentLoaded", function () {
-//   setTimeout(function () {
-//     document.getElementById("loading-container").style.transform =
-//       "translateY(-110%)";
-//     document.querySelector(".color-block-1").style.transform = "scaleY(5)";
-//     document.querySelector(".color-block-2").style.transform = "scaleY(3.5)";
-//     setTimeout(function () {
-//       document.querySelector(".color-block-1").style.transform = "scaleY(0)";
-//       document.querySelector(".color-block-2").style.transform = "scaleY(0)";
-//     }, 700);
-//   }, 2000);
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById("loading-container").style.transform =
+      "translateY(-110%)";
+    document.querySelector(".color-block-1").style.transform = "scaleY(5)";
+    document.querySelector(".color-block-2").style.transform = "scaleY(3.5)";
+    setTimeout(function () {
+      document.querySelector(".color-block-1").style.transform = "scaleY(0)";
+      document.querySelector(".color-block-2").style.transform = "scaleY(0)";
+    }, 700);
+  }, 2000);
+});
 
 // Top Icon
 const topIcon = document.querySelector("#top-icon");
